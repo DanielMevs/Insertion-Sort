e0eabd7be59f19a58c00810dc8aeebc428465ea5
danny <dannymevs@hotmail.com>
2018-11-12 20:53:53 -0500

    fix symatic error in python file

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,7 +1,12 @@
 arr = [3,44,5,47,15,36,26,27,2,46]
 print("array = ", arr)
 
-
+def swap(arr, i , j):
+    temp = arr[j]
+    arr[j] = arr[i]
+    arr[i] = temp
+    return arr
+    
 i = 1
 temp = 0
 n = len(arr)
@@ -9,12 +14,14 @@ while i < n:
     current = arr[i]
     j = i - 1
     while j >= 0:
-        if arr[i] < arr[j]:
-            temp = arr[j]
+        if current < arr[j]:
+            arr = swap(arr,j,j+1)
+            '''temp = arr[j]
             arr[j] = arr[i]
-            arr[i] = temp
+            arr[i] = temp'''
             print(arr)
         j = j -1
+        current = arr[j+1]
     i = i + 1
 
 
